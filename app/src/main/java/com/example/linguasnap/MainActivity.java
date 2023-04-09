f110c572ce99546253423bafa272da99fc9b67f3
ismekakawin9 <ntdat1232001@gmail.com>
2023-04-09 16:43:40 +0700

    Update ImageToText function

--- app/src/main/java/com/example/linguasnap/MainActivity.java
+++ app/src/main/java/com/example/linguasnap/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.linguasnap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView iv_camera_option;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv_camera_option=findViewById(R.id.iv_camera_option);
+        iv_camera_option.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent= new Intent(MainActivity.this,ImageToTextActivity.class);
+                startActivity(cameraIntent);
+            }
+        });
     }
 }
