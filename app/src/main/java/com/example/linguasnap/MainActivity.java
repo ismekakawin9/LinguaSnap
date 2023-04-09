3d9d51437a2c1fa0cdc312549734edfc73f440ab
Edward Kenway <winkha14567@gmail.com>
2023-04-09 16:08:18 +0700

    Translate

--- app/src/main/java/com/example/linguasnap/MainActivity.java
+++ app/src/main/java/com/example/linguasnap/MainActivity.java
@@ -2,13 +2,159 @@ package com.example.linguasnap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.translate.Detection;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText EnterText;
+    private TextView Translated;
+    private String originalText;
+    private String translatedText;
+    private boolean connected;
+    TranslateFrom tf = new TranslateFrom();
+    TranslateTo to = new TranslateTo();
+    String SelectedLanguage;
+    String BaseLanguage;
+    String detectedLanguage;
+    Translate translate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String [] languages = getResources().getStringArray(R.array.LanguageFrom);
+        TextView TextFrom = findViewById(R.id.TextFrom);
+        TextView TextTo = findViewById(R.id.TextTo);
+        Spinner SpinnerFrom = (Spinner) findViewById(R.id.SpinnerFrom);
+        Spinner SpinnerTo = (Spinner) findViewById(R.id.SpinnerTo);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.LanguageFrom, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.LanguageTo, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SpinnerFrom.setAdapter(adapter1);
+        SpinnerFrom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                BaseLanguage = adapterView.getItemAtPosition(i).toString();
+                String Language = adapterView.getItemAtPosition(i).toString();
+                TextFrom.setText(Language);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        SpinnerTo.setAdapter(adapter2);
+        SpinnerTo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                SelectedLanguage = adapterView.getItemAtPosition(i).toString();
+                String LanguageTemp = adapterView.getItemAtPosition(i).toString();
+                TextTo.setText(LanguageTemp);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        EnterText = findViewById(R.id.EnterText);
+        Translated = findViewById(R.id.Translated);
+        Button btnTranslate = findViewById(R.id.btnTranslate);
+        btnTranslate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkInternetConnection()){
+                    getTranslateService();
+                    translate();
+                    LanguageDetect();
+                } else{
+                    Translated.setText("No internet connection");
+                }
+                EnterText.onEditorAction(EditorInfo.IME_ACTION_DONE);
+                TextFrom.setText(LanguageDetect());
+                int idLanguage = Arrays.asList(languages).indexOf(LanguageDetect());
+                SpinnerFrom.setSelection(idLanguage);
+
+            }
+
+        });
     }
+    public void getTranslateService() {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try (InputStream is = getResources().openRawResource(R.raw.credentials)) {
+
+            //Get credentials:
+            final GoogleCredentials myCredentials = GoogleCredentials.fromStream(is);
+
+            //Set credentials and get translate service:
+            TranslateOptions translateOptions = TranslateOptions.newBuilder().setCredentials(myCredentials).build();
+            translate = translateOptions.getService();
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+
+        }
+    }
+    public void translate() {
+
+
+        //Get input text to be translated:
+        originalText = EnterText.getText().toString();
+        Translation translation = translate.translate(originalText, Translate.TranslateOption.targetLanguage(to.selectCountry(SelectedLanguage)), Translate.TranslateOption.model(tf.selectCountry(BaseLanguage)));
+        translatedText = translation.getTranslatedText();
+        //Translated text and original text are set to TextViews:
+        Translated.setText(translatedText);
+    }
+
+    public String LanguageDetect(){
+        //int duration = Toast.LENGTH_SHORT;
+        //Context context = getApplicationContext();
+        originalText = EnterText.getText().toString();
+        Detection detection = translate.detect(originalText);
+        detectedLanguage = detection.getLanguage();
+        /*Toast toast = Toast.makeText(context,detectedLanguage,duration);
+        toast.show();*/
+        return tf.detect(detectedLanguage);
+    }
+    public boolean checkInternetConnection() {
+
+        //Check internet connection:
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        //Means that we are connected to a network (mobile or wi-fi)
+        connected = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED;
+
+        return connected;
+    }
+
 }
---
+++ app/src/main/java/com/example/linguasnap/TranslateFrom.java
@@ -0,0 +1,434 @@
+package com.example.linguasnap;
+
+public class TranslateFrom {
+    public String selectCountry(String Country) {
+        String text = "based";
+        switch (Country){
+            case "Afrikaans":
+                text ="af";
+                break;
+            case "Albanian":
+                text ="sq";
+                break;
+            case "Amharic":
+                text ="am";
+                break;
+            case "Arabic":
+                text ="ar";
+                break;
+            case "Armenian":
+                text ="hy";
+                break;
+            case "Assamese":
+                text ="as";
+                break;
+            case "Aymara":
+                text ="ay";
+                break;
+            case "Azerbaijani":
+                text ="az";
+                break;
+            case "Bambara":
+                text ="bm";
+                break;
+            case "Basque":
+                text ="eu";
+                break;
+            case "Belarusian":
+                text ="be";
+                break;
+            case "Bengali":
+                text ="bn";
+                break;
+            case "Bhojpuri":
+                text ="bho";
+                break;
+            case "Bosnian":
+                text ="bs";
+                break;
+            case "Bulgarian":
+                text ="bg";
+                break;
+            case "Catalan":
+                text ="ca";
+                break;
+            case "Cebuano":
+                text ="ceb";
+                break;
+            case "Chinese (Simplified)":
+                text ="zh";
+                break;
+            case "Chinese (Traditional)":
+                text ="zh-TW";
+                break;
+            case "Corsican":
+                text ="co";
+                break;
+            case "Croatian":
+                text ="hr";
+                break;
+            case "Czech":
+                text ="cs";
+                break;
+            case "Danish":
+                text ="da";
+                break;
+            case "Dhivehi":
+                text ="dv";
+                break;
+            case "Dogri":
+                text ="doi";
+                break;
+            case "Dutch":
+                text ="nl";
+                break;
+            case "English":
+                text ="en";
+                break;
+            case "Esperanto":
+                text ="eo";
+                break;
+            case "Estonian":
+                text ="et";
+                break;
+            case "Ewe":
+                text ="ee";
+                break;
+            case "Filipino (Tagalog)":
+                text ="fil";
+                break;
+            case "Finnish":
+                text ="fi";
+                break;
+            case "French":
+                text ="fr";
+                break;
+            case "Frisian":
+                text ="fy";
+                break;
+            case "Galician":
+                text ="gl";
+                break;
+            case "Georgian":
+                text ="ka";
+                break;
+            case "German":
+                text ="de";
+                break;
+            case "Greek":
+                text ="el";
+                break;
+            case "Guarani":
+                text ="gn";
+                break;
+            case "Gujarati":
+                text ="gu";
+                break;
+            case "Haitian Creole":
+                text ="ht";
+                break;
+            case "Hausa":
+                text ="ha";
+                break;
+            case "Hawaiian":
+                text ="haw";
+                break;
+            case "Hebrew":
+                text ="he";
+                break;
+            case "Hindi":
+                text ="hi";
+                break;
+            case "Hmong":
+                text ="hmn";
+                break;
+            case "Hungarian":
+                text ="hu";
+                break;
+            case "Icelandic":
+                text ="is";
+                break;
+            case "Igbo":
+                text ="ig";
+                break;
+            case "Ilocano":
+                text ="ilo";
+                break;
+            case "Indonesian":
+                text ="id";
+                break;
+            case "Irish":
+                text ="ga";
+                break;
+            case "Italian":
+                text ="it";
+                break;
+            case "Japanese":
+                text ="ja";
+                break;
+            case "Javanese":
+                text ="jv";
+                break;
+            case "Kannada":
+                text ="kn";
+                break;
+            case "Kazakh":
+                text ="kk";
+                break;
+            case "Khmer":
+                text ="km";
+                break;
+            case "Kinyarwanda":
+                text ="rw";
+                break;
+            case "Konkani":
+                text ="gom";
+                break;
+            case "Korean":
+                text ="ko";
+                break;
+            case "Krio":
+                text ="kri";
+                break;
+            case "Kurdish":
+                text ="ku";
+                break;
+            case "Kurdish (Sorani)":
+                text ="ckb";
+                break;
+            case "Kyrgyz":
+                text ="ky";
+                break;
+            case "Lao":
+                text ="lo";
+                break;
+            case "Latin":
+                text ="la";
+                break;
+            case "Latvian":
+                text ="lv";
+                break;
+            case "Lingala":
+                text ="ln";
+                break;
+            case "Lithuanian":
+                text ="lt";
+                break;
+            case "Luganda":
+                text ="lg";
+                break;
+            case "Luxembourgish":
+                text ="lb";
+                break;
+            case "Macedonian":
+                text ="mk";
+                break;
+            case "Maithili":
+                text ="mai";
+                break;
+            case "Malagasy":
+                text ="mg";
+                break;
+            case "Malay":
+                text ="ms";
+                break;
+            case "Malayalam":
+                text ="ml";
+                break;
+            case "Maltese":
+                text ="mt";
+                break;
+            case "Maori":
+                text ="mi";
+                break;
+            case "Marathi":
+                text ="mr";
+                break;
+            case "Meiteilon (Manipuri)":
+                text ="mni-Mtei";
+                break;
+            case "Mizo":
+                text ="lus";
+                break;
+            case "Mongolian":
+                text ="mn";
+                break;
+            case "Myanmar (Burmese)":
+                text ="my";
+                break;
+            case "Nepali":
+                text ="ne";
+                break;
+            case "Norwegian":
+                text ="no";
+                break;
+            case "Nyanja (Chichewa)":
+                text ="ny";
+                break;
+            case "Odia (Oriya)":
+                text ="or";
+                break;
+            case "Oromo":
+                text ="om";
+                break;
+            case "Pashto":
+                text ="ps";
+                break;
+            case "Persian":
+                text ="fa";
+                break;
+            case "Polish":
+                text ="pl";
+                break;
+            case "Portuguese (Portugal, Brazil)":
+                text ="pt";
+                break;
+            case "Punjabi":
+                text ="pa";
+                break;
+            case "Quechua":
+                text ="qu";
+                break;
+            case "Romanian":
+                text ="ro";
+                break;
+            case "Russian":
+                text ="ru";
+                break;
+            case "Samoan":
+                text ="sm";
+                break;
+            case "Sanskrit":
+                text ="sa";
+                break;
+            case "Scots Gaelic":
+                text ="gd";
+                break;
+            case "Sepedi":
+                text ="nso";
+                break;
+            case "Serbian":
+                text ="sr";
+                break;
+            case "Sesotho":
+                text ="st";
+                break;
+            case "Shona":
+                text ="sn";
+                break;
+            case "Sindhi":
+                text ="sd";
+                break;
+            case "Sinhala (Sinhalese)":
+                text ="si";
+                break;
+            case "Slovak":
+                text ="sk";
+                break;
+            case "Slovenian":
+                text ="sl";
+                break;
+            case "Somali":
+                text ="so";
+                break;
+            case "Spanish":
+                text ="es";
+                break;
+            case "Sundanese":
+                text ="su";
+                break;
+            case "Swahili":
+                text ="sw";
+                break;
+            case "Swedish":
+                text ="sv";
+                break;
+            case "Tagalog (Filipino)":
+                text ="tl";
+                break;
+            case "Tajik":
+                text ="tg";
+                break;
+            case "Tamil":
+                text ="ta";
+                break;
+            case "Tatar":
+                text ="tt";
+                break;
+            case "Telugu":
+                text ="te";
+                break;
+            case "Thai":
+                text ="th";
+                break;
+            case "Tigrinya":
+                text ="ti";
+                break;
+            case "Tsonga":
+                text ="ts";
+                break;
+            case "Turkish":
+                text ="tr";
+                break;
+            case "Turkmen":
+                text ="tk";
+                break;
+            case "Twi (Akan)":
+                text ="ak";
+                break;
+            case "Ukrainian":
+                text ="uk";
+                break;
+            case "Urdu":
+                text ="ur";
+                break;
+            case "Uyghur":
+                text ="ug";
+                break;
+            case "Uzbek":
+                text ="uz";
+                break;
+            case "Vietnamese":
+                text ="vi";
+                break;
+            case "Welsh":
+                text ="cy";
+                break;
+            case "Xhosa":
+                text ="xh";
+                break;
+            case "Yiddish":
+                text ="yi";
+                break;
+            case "Yoruba":
+                text ="yo";
+                break;
+            case "Zulu":
+                text ="zu";
+                break;
+            case "Auto Detect":
+                text ="base";
+                break;
+        }
+        return text;
+    }
+    public String detect (String Language){
+        String text = "based";
+        switch(Language){
+            case "vi":
+                text ="Vietnamese";
+                break;
+            case "en":
+                text = "English";
+                break;
+            case "it":
+                text ="Italian";
+                break;
+            case "cn":
+                text ="Chinese";
+                break;
+        }
+        return text;
+    }
+}
+
